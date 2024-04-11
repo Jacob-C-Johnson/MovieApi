9908056099e172ae291327cf6550ca0a08fa1ac9
Jacob-C-Johnson <jacobj1789@gmail.com>
2024-04-11 16:23:02 -0400

    please work

--- movieapi/obj/Debug/net8.0/movieapi.AssemblyInfo.cs
+++ movieapi/obj/Debug/net8.0/movieapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movieapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e23e58f3c5b3a5901c3be224d8a2120da8be29ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef2d7b585e9c7b7235e5cc7453ba49141e47f4e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("movieapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movieapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
